4e993b4d04e73d343ed6484c66539623a04cd3df
devotion <303054578@qq.com>
2017-05-27 06:22:47 +0800

    加入jetbrick模板引擎

--- blade-core/src/main/java/com/blade/mvc/view/ModelAndView.java
+++ blade-core/src/main/java/com/blade/mvc/view/ModelAndView.java
@@ -15,10 +15,9 @@
  */
 package com.blade.mvc.view;
 
-import com.blade.kit.CollectionKit;
-
-import java.util.HashMap;
 import java.util.Map;
+
+import com.blade.kit.CollectionKit;
 
 /**
  * ModelAndView, Using templates and data
@@ -28,95 +27,103 @@ import java.util.Map;
  */
 public class ModelAndView {
 
-    /**
-     * Data object, the object is placed in the attribute httprequest
-     */
-    private Map<String, Object> model = new HashMap<>(8);
+	/**
+	 * Data object, the object is placed in the attribute httprequest
+	 */
+	private Map<String, Object> model = CollectionKit.newHashMap(8);
 
-    /**
-     * View Page
-     */
-    private String view;
+	/**
+	 * View Page
+	 */
+	private String view;
 
-    public ModelAndView() {
-    }
+	public ModelAndView() {
+	}
 
-    /**
-     * Create an empty view
-     *
-     * @param view view page
-     */
-    public ModelAndView(String view) {
-        super();
-        this.model = CollectionKit.newHashMap();
-        this.view = view;
-    }
+	/**
+	 * Create an empty view
+	 *
+	 * @param view
+	 *            view page
+	 */
+	public ModelAndView(String view) {
+		super();
+		this.model = CollectionKit.newHashMap();
+		this.view = view;
+	}
 
-    /**
-     * Create a model view object with data
-     *
-     * @param model model data
-     * @param view  view page
-     */
-    public ModelAndView(Map<String, Object> model, String view) {
-        super();
-        this.model = model;
-        this.view = view;
-    }
+	/**
+	 * Create a model view object with data
+	 *
+	 * @param model
+	 *            model data
+	 * @param view
+	 *            view page
+	 */
+	public ModelAndView(Map<String, Object> model, String view) {
+		super();
+		this.model = model;
+		this.view = view;
+	}
 
-    /**
-     * Add data to model
-     *
-     * @param key   key
-     * @param value value
-     */
-    public void add(String key, Object value) {
-        this.model.put(key, value);
-    }
+	/**
+	 * Add data to model
+	 *
+	 * @param key
+	 *            key
+	 * @param value
+	 *            value
+	 */
+	public void add(String key, Object value) {
+		this.model.put(key, value);
+	}
 
-    /**
-     * Remove model data
-     *
-     * @param key key
-     */
-    public void remove(String key) {
-        this.model.remove(key);
-    }
+	/**
+	 * Remove model data
+	 *
+	 * @param key
+	 *            key
+	 */
+	public void remove(String key) {
+		this.model.remove(key);
+	}
 
-    /**
-     * @return Return view page
-     */
-    public String getView() {
-        return view;
-    }
+	/**
+	 * @return Return view page
+	 */
+	public String getView() {
+		return view;
+	}
 
-    /**
-     * Setting view page
-     *
-     * @param view view page
-     */
-    public void setView(String view) {
-        this.view = view;
-    }
+	/**
+	 * Setting view page
+	 *
+	 * @param view
+	 *            view page
+	 */
+	public void setView(String view) {
+		this.view = view;
+	}
 
-    /**
-     * @return Return model map
-     */
-    public Map<String, Object> getModel() {
-        return model;
-    }
+	/**
+	 * @return Return model map
+	 */
+	public Map<String, Object> getModel() {
+		return model;
+	}
 
-    /**
-     * Setting model
-     *
-     * @param model Storage data map
-     */
-    public void setModel(Map<String, Object> model) {
-        this.model = model;
-    }
+	/**
+	 * Setting model
+	 *
+	 * @param model
+	 *            Storage data map
+	 */
+	public void setModel(Map<String, Object> model) {
+		this.model = model;
+	}
 
-    @Override
-    public String toString() {
-        return "view = " + view + ", model = " + model;
-    }
+	@Override
+	public String toString() {
+		return "view = " + view + ", model = " + model;
+	}
 }
